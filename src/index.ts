337a227ecbde418ba2291ff1da08f8a330da080a
Ranjith Zachariah <ranjzach@amazon.com>
2023-02-24 16:43:45 -0500

    Add airbnb-base

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-const bag = { fruit: "apple", vegetable: "carrot" };
-console.log("bag", bag);
+const bag = { fruit: 'apple', vegetable: 'carrot' };
+console.log('bag', bag);
 
 const fruitFoods = [
-  "apple juice",
-  "orange marmalade",
-  "pear tart",
-  "grape crush",
-  "pinot colada",
+  'apple juice',
+  'orange marmalade',
+  'pear tart',
+  'grape crush',
+  'pinot colada',
 ];
-console.log("fruitFoods", fruitFoods);
+console.log('fruitFoods', fruitFoods);
 
 function sayMyName(name: string): void {
-  if (name === "Heisenberg") {
+  if (name === 'Heisenberg') {
     console.log("You're right 👍");
   } else {
     console.log("You're wrong 👎");
   }
 }
 
-sayMyName("Heisenberg");
+sayMyName('Heisenberg');
